25ebd880aac7f7a81fae5ccbe2b4a1eba788ab2e
Rishi Sinha <rishi.z.sinha@gmail.com>
2015-04-16 17:38:48 -0700

    clean charquote js

---
+++ scripts/postgresconnect.java
@@ -0,0 +1,9 @@
+private static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    return DriverManager.getConnection(dbUrl, username, password);
+}
